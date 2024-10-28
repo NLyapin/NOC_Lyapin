4710b4f192be6282428da2750c2a7287361c1c1d
Никита Ляпин <nikitalapin@MacBook-Air-Nikita.local>
2024-10-28 14:44:33 +0700

    added test script

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/NLyapin/NOC_Lyapin/internal/devices"
 	"github.com/NLyapin/NOC_Lyapin/pkg/config"
-
-	"golang.org/x/crypto/ssh"
 )
 
 func main() {
@@ -46,57 +43,4 @@ func main() {
 
 	// Удаление конфигурации
 	configStore.DeleteConfig("Router1")
-}
-
-func QoS() {
-	// Параметры подключения SSH
-	sshHost := "10.25.16.254:62214"
-	sshUser := "user"
-	sshPassword := "user6501"
-
-	// Настройки SSH клиента
-	config := &ssh.ClientConfig{
-		User: sshUser,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(sshPassword),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-
-	// Подключение к роутеру
-	client, err := ssh.Dial("tcp", sshHost, config)
-	if err != nil {
-		log.Fatalf("Ошибка подключения: %s", err)
-	}
-	defer client.Close()
-
-	// Создание SSH сессии
-	session, err := client.NewSession()
-	if err != nil {
-		log.Fatalf("Ошибка создания сессии: %s", err)
-	}
-	defer session.Close()
-
-	// Команды для настройки QoS
-	command := `
-    configure terminal
-    qos profile name HighPriority
-    qos priority high
-    qos profile name LowPriority
-    qos priority low
-    interface ethernet 0/1
-    qos apply profile HighPriority
-    commit
-    show qos configuration
-    `
-
-	// Выполнение команды и получение вывода
-	output, err := session.CombinedOutput(command)
-	if err != nil {
-		log.Fatalf("Ошибка выполнения команды: %s", err)
-	}
-
-	// Вывод результата
-	fmt.Println("Результат настройки QoS:")
-	fmt.Println(string(output))
 }
